99a175cc962d14d75918351b0e3706e3db6e1f62
youxia <1841079@qq.com>
2016-10-17 20:11:55 +0800

    补充一下上次没提交上的 count 函数

--- SecurityDemo/src/com/xkland/sample/SecurityDemo.java
+++ SecurityDemo/src/com/xkland/sample/SecurityDemo.java
@@ -1,22 +1,40 @@
 package com.xkland.sample;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class SecurityDemo {
 
 	public static void main(String[] args) {
-		if(args.length == 1 && args[0] == "getProps"){
+		if(args.length == 1 && args[0].equalsIgnoreCase("getProps")){
 			getProps();
-		}else if(args.length == 2 && args[0] == "count"){
+		}else if(args.length == 2 && args[0].equalsIgnoreCase("count")){
 			count(args[1]);
 		}else{
 			usage();
 		}
 	}
 	
+	private static void count(String filename) {
+		int count = 0;
+		try(InputStream in = new FileInputStream(filename)){
+			while(in.read() != -1){
+				count++;
+			}
+			System.out.println("文件 " + filename + " 含有 " + count + " 个字节。");
+		} catch (IOException e) {
+			System.err.println("产生异常：" + e.toString());
+		}
+		
+	}
+
 	public static void usage(){
 		System.out.println("Usage:");
 		System.out.println("    java com.xkland.sample.SecurityDemo getProps");
 		System.out.println("  or");
 		System.out.println("    java com.xkland.sample.SecurityDemo count filename");
+		System.out.println();
 	}
 
 	public static void getProps(){
